851a95b29b9c484368c0990167cf4757c3150aa9
gigitux <gigitux@gmail.com>
2021-01-18 20:01:42 +0100

    rename file

--- apps/listpeople-fe/src/app/components/users-list-smart/Users-list-smart.tsx
+++ apps/listpeople-fe/src/app/components/users-list-smart/Users-list-smart.tsx
@@ -4,15 +4,31 @@ import { getAllUsers } from '../../services/user.services';
 import { UsersListDumb } from '../users-list-dumb/Users-list-dumb';
 
 export function UsersListSmart() {
-  const [users, setUsers] = useState([] as Array<UserInfo>);
+  const [usersInfo, setUsers] = useState({
+    users: [] as Array<UserInfo>,
+    error: false,
+    isLoading: true,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getAllUsers();
-      setUsers(result.data);
+      try {
+        const result = await getAllUsers();
+        setUsers({ users: result.data, error: false, isLoading: false });
+      } catch (err) {
+        setUsers({ users: [], error: true, isLoading: false });
+      }
     };
     fetchData();
   }, []);
 
-  return <UsersListDumb usersList={users}></UsersListDumb>;
+  if (usersInfo.isLoading) {
+    return <span>Loading</span>;
+  } else {
+    return usersInfo.error ? (
+      <span>Ooop, there is an error</span>
+    ) : (
+      <UsersListDumb usersList={usersInfo.users}></UsersListDumb>
+    );
+  }
 }
